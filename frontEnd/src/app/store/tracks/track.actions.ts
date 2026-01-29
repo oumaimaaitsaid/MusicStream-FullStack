fc0d2d2b8aac928d94d90c577b8f5de460e65624
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-01-29 15:05:10 +0100

    feat(store): add loadTracksSuccess action with tracks payload

--- frontEnd/src/app/store/tracks/track.actions.ts
+++ frontEnd/src/app/store/tracks/track.actions.ts
@@ -2,3 +2,8 @@ import { createAction, props } from '@ngrx/store';
 import { Track } from '../../core/models/track.model';
 
 export const loadTracks = createAction('[Track] Load Tracks');
+
+export const loadTracksSuccess = createAction(
+  '[Track] Load Tracks Success',
+  props<{ tracks: Track[] }>()
+);
