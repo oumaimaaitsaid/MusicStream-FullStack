d655898dbba46b935c0069df50483b7fb39efb4a
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-01-29 16:34:03 +0100

    feat(app.config): add providers

--- frontEnd/src/app/app.config.ts
+++ frontEnd/src/app/app.config.ts
@@ -2,10 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { trackReducer } from './store/tracks/track.reducer';
+import { TrackEffects } from './store/tracks/track.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideStore({ tracks: trackReducer }),
+    provideEffects(TrackEffects)
   ]
 };
