f4dbb1b3ad41fcc2503610807a648fc9836f1615
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-01-29 20:50:08 +0100

    feat(library): change the signals to observables NgRx

--- frontEnd/src/app/features/library/library.component.ts
+++ frontEnd/src/app/features/library/library.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
 import { TrackService } from '../../core/services/track.service';
 import { AudioPlayerService } from '../../core/services/audio-player.service';
 import { FilterPipe } from '../../shared/pipes/filter.pipe';
 import { Track } from '../../core/models/track.model';
+import * as TrackActions from '../../store/tracks/track.actions';
+import { selectAllTracks } from '../../store/tracks/track.selectors';
 
 @Component({
   standalone: true,
@@ -12,17 +15,21 @@ import { Track } from '../../core/models/track.model';
   styleUrl: './library.component.css'
 })
 export class LibraryComponent implements OnInit {
-  public trackService = inject(TrackService);
+  public store = inject(Store);
   public player = inject(AudioPlayerService);
 
+
+   public tracks$ =this.store.select(selectAllTracks);
   ngOnInit() {
-    this.trackService.loadTracks();
+    this.store.dispatch(TrackActions.loadTracks());
   }
 onEdit(track: Track) {
   const newTitle = prompt("Nouveau titre :", track.title);
   if (newTitle) {
     const updatedTrack = { ...track, title: newTitle };
-    this.trackService.updateTrack(updatedTrack);
+
   }
 }
+onDelete(id :string){
+  }
 }
